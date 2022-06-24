313d7f90db4109f6b494f6dca4df56edcef65f70
Marcus Deans <marcus.deans@duke.edu>
2022-06-24 16:53:46 -0400

    Attempted AWS updates

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ from sqlalchemy.dialects.postgresql import JSONB
 import os
 import urllib
 
-# DATABASE_URL = "sqlite:///./test.db"
+DATABASE_URL = "sqlite:///./test.db"
 
 # host_server = os.environ.get('host_server', 'localhost')
 # db_server_port = urllib.parse.quote_plus(str(os.environ.get('db_server_port', '5432')))
@@ -24,7 +24,7 @@ import urllib
 # ssl_mode = urllib.parse.quote_plus(str(os.environ.get('ssl_mode','require')))
 # DATABASE_URL = 'postgresql://{}:{}@{}:{}/{}?sslmode={}'.format(db_username, db_password, host_server, db_server_port, database_name, ssl_mode)
 # DATABASE_URL = "postgresql://tyhogopgeybpov:b1bfaf71b3aaa3c7db86ce5eb4630b6393ee82a39f25f22fdd23baa2a4cd1225@ec2-34-200-35-222.compute-1.amazonaws.com:5432/dfac6s87cm7bgo"
-DATABASE_URL = "postgresql://owwrqgudottkpb:7281b28a9cdce0f90e2b00702d7d6289185ff262a49209bc8884e5214d2d2915@ec2-3-224-8-189.compute-1.amazonaws.com:5432/d6vjnmvf7pp92v"
+# DATABASE_URL = "postgresql://owwrqgudottkpb:7281b28a9cdce0f90e2b00702d7d6289185ff262a49209bc8884e5214d2d2915@ec2-3-224-8-189.compute-1.amazonaws.com:5432/d6vjnmvf7pp92v"
 
 database = databases.Database(DATABASE_URL)
 
@@ -71,17 +71,17 @@ relationships = sqlalchemy.Table(
 # )
 
 # LOCAL version
-# engine = sqlalchemy.create_engine(
-#     DATABASE_URL, connect_args={"check_same_thread": False}
-# )
-# metadata.create_all(engine)
-
-
-# PostgreSQL version
 engine = sqlalchemy.create_engine(
-    DATABASE_URL
+    DATABASE_URL, connect_args={"check_same_thread": False}
 )
 metadata.create_all(engine)
+
+
+# # PostgreSQL version
+# engine = sqlalchemy.create_engine(
+#     DATABASE_URL
+# )
+# metadata.create_all(engine)
 
 
 # Professional parameters used in both creation, updates, etc.
